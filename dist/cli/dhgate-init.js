36e397f4023e5c720b91b47ff81494931f156d49
rabrux <n3ur0n4@gmail.com>
2017-12-14 13:36:09 -0600

    dhgate init command npm init and install dev dependencies

--- dist/cli/dhgate-init.js
+++ dist/cli/dhgate-init.js
@@ -1,5 +1,5 @@
 (function() {
-  var args, colors, flags, fs, gatePath, path;
+  var args, child, cmd, colors, configure, exec, execSync, flags, fs, packageFile, path, stream;
 
   args = require('args');
 
@@ -9,32 +9,67 @@
 
   colors = require('colors');
 
+  exec = require('child_process').spawn;
+
+  execSync = require('child_process').spawnSync;
+
+  stream = require('stream');
+
   args.option('src', 'app source directory', 'src').option('dist', 'app production directory', 'dist').option('port', 'app listen port', 1337).example('dhgate init --src source --dist bundle --port 1234', 'initializes new project on directory \"source\" and listen port \"1234\"');
 
   flags = args.parse(process.argv);
 
-  if (!fs.existsSync(flags.src)) {
-    fs.mkdirSync(flags.src);
+  packageFile = path.join(process.cwd(), 'package.json');
+
+  cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+
+  if (!fs.existsSync(packageFile)) {
+    child = exec(cmd, ['init'], {
+      stdio: [0, 'pipe', 'pipe']
+    });
+    child.stdout.on('data', function(data) {
+      return process.stdout.write(data.toString());
+    });
+    child.stderr.on('data', function(data) {
+      return console.log('err', data.toString());
+    });
+    child.on('close', function(code) {
+      return configure();
+    });
+  } else {
+    configure();
   }
 
-  console.log('->'.green, 'application directory created at', flags.src.cyan);
-
-  fs.writeFileSync('.dhgate.json', JSON.stringify({
-    root: flags.src,
-    dist: flags.dist,
-    port: flags.port
-  }, null, 2));
-
-  console.log('->'.green, 'configuration file created as', '.dhgate.json'.cyan);
-
-  gatePath = path.join(process.cwd(), flags.src, 'gate.coffee');
-
-  fs.createReadStream(path.join(process.cwd(), 'node_modules', 'dhgate', 'assets', 'gate.coffee')).pipe(fs.createWriteStream(gatePath));
-
-  console.log('->'.green, 'gate index file created at', flags.src.cyan);
-
-  fs.createReadStream(path.join(process.cwd(), 'node_modules', 'dhgate', 'assets', 'Gulpfile.coffee')).pipe(fs.createWriteStream(path.join(process.cwd(), 'Gulpfile.coffee')));
-
-  console.log('->'.green, 'gulpfile created, use "gulp" command to compile your app and "gulp dev" to watch and recompile');
+  configure = function() {
+    var gatePath;
+    if (!fs.existsSync(flags.src)) {
+      fs.mkdirSync(flags.src);
+    }
+    console.log('->'.green, 'application directory created at', flags.src.cyan);
+    fs.writeFileSync('.dhgate.json', JSON.stringify({
+      root: flags.src,
+      dist: flags.dist,
+      port: flags.port
+    }, null, 2));
+    console.log('->'.green, 'configuration file created as', '.dhgate.json'.cyan);
+    gatePath = path.join(process.cwd(), flags.src, 'gate.coffee');
+    fs.createReadStream(path.join(process.cwd(), 'node_modules', 'dhgate', 'assets', 'gate.coffee')).pipe(fs.createWriteStream(gatePath));
+    console.log('->'.green, 'gate index file created at', flags.src.cyan);
+    fs.createReadStream(path.join(process.cwd(), 'node_modules', 'dhgate', 'assets', 'Gulpfile.coffee')).pipe(fs.createWriteStream(path.join(process.cwd(), 'Gulpfile.coffee')));
+    console.log('->'.green, 'gulpfile created, use "gulp" command to compile your app and "gulp dev" to watch and recompile');
+    console.log('->'.green, 'installing node dev dependencies');
+    child = exec(cmd, ['install', '--save-dev', 'gulp', 'gulp-coffee', 'gulp-watch', 'coffeescript'], {
+      stdio: [0, 'pipe', 'pipe']
+    });
+    child.stdout.on('data', function(data) {
+      return process.stdout.write(data.toString());
+    });
+    child.stderr.on('data', function(data) {
+      return console.log('err', data.toString());
+    });
+    return child.on('close', function(code) {
+      return console.log('->'.green, 'dev dependencies installed');
+    });
+  };
 
 }).call(this);
--- src/cli/dhgate-init.coffee
+++ src/cli/dhgate-init.coffee
@@ -3,6 +3,10 @@ path   = require 'path'
 fs     = require 'fs'
 colors = require 'colors'
 
+exec     = require( 'child_process' ).spawn
+execSync = require( 'child_process' ).spawnSync
+stream   = require 'stream'
+
 args
   .option 'src', 'app source directory', 'src'
   .option 'dist', 'app production directory', 'dist'
@@ -11,29 +15,64 @@ args
 
 flags = args.parse process.argv
 
-# check src directory
-if not fs.existsSync( flags.src )
-  fs.mkdirSync flags.src
+# verify npm file
+packageFile = path.join process.cwd(), 'package.json'
+cmd = if /^win/.test( process.platform ) then 'npm.cmd' else 'npm'
+if not fs.existsSync( packageFile )
+  child = exec cmd, [ 'init' ],
+    stdio : [
+      0
+      'pipe'
+      'pipe'
+    ]
+  child.stdout.on 'data', ( data ) ->
+    process.stdout.write data.toString()
+  child.stderr.on 'data', ( data ) ->
+    console.log 'err', data.toString()
+  child.on 'close', ( code ) ->
+    configure()
+else
+  configure()
 
-console.log '->'.green, 'application directory created at', flags.src.cyan
+configure = ->
+  # check src directory
+  if not fs.existsSync( flags.src )
+    fs.mkdirSync flags.src
 
-# write config file
-fs.writeFileSync '.dhgate.json', JSON.stringify( { root : flags.src, dist: flags.dist, port : flags.port }, null, 2 )
+  console.log '->'.green, 'application directory created at', flags.src.cyan
 
-console.log '->'.green, 'configuration file created as', '.dhgate.json'.cyan
+  # write config file
+  fs.writeFileSync '.dhgate.json', JSON.stringify( { root : flags.src, dist: flags.dist, port : flags.port }, null, 2 )
 
-# copy base gate to source dir
-gatePath = path.join process.cwd(), flags.src, 'gate.coffee'
+  console.log '->'.green, 'configuration file created as', '.dhgate.json'.cyan
 
-fs
-  .createReadStream path.join process.cwd(), 'node_modules', 'dhgate', 'assets', 'gate.coffee'
-  .pipe fs.createWriteStream gatePath
+  # copy base gate to source dir
+  gatePath = path.join process.cwd(), flags.src, 'gate.coffee'
 
-console.log '->'.green, 'gate index file created at', flags.src.cyan
+  fs
+    .createReadStream path.join process.cwd(), 'node_modules', 'dhgate', 'assets', 'gate.coffee'
+    .pipe fs.createWriteStream gatePath
 
-# copy gulpfile
-fs
-  .createReadStream path.join process.cwd(), 'node_modules', 'dhgate', 'assets', 'Gulpfile.coffee'
-  .pipe fs.createWriteStream path.join process.cwd(), 'Gulpfile.coffee'
+  console.log '->'.green, 'gate index file created at', flags.src.cyan
 
-console.log '->'.green, 'gulpfile created, use "gulp" command to compile your app and "gulp dev" to watch and recompile'
+  # copy gulpfile
+  fs
+    .createReadStream path.join process.cwd(), 'node_modules', 'dhgate', 'assets', 'Gulpfile.coffee'
+    .pipe fs.createWriteStream path.join process.cwd(), 'Gulpfile.coffee'
+
+  console.log '->'.green, 'gulpfile created, use "gulp" command to compile your app and "gulp dev" to watch and recompile'
+
+  # add dev packages to package.json
+  console.log '->'.green, 'installing node dev dependencies'
+  child = exec cmd, [ 'install', '--save-dev', 'gulp', 'gulp-coffee', 'gulp-watch', 'coffeescript' ],
+    stdio : [
+      0
+      'pipe'
+      'pipe'
+    ]
+  child.stdout.on 'data', ( data ) ->
+    process.stdout.write data.toString()
+  child.stderr.on 'data', ( data ) ->
+    console.log 'err', data.toString()
+  child.on 'close', ( code ) ->
+    console.log '->'.green, 'dev dependencies installed'
