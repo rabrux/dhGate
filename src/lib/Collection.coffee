a895b2bdd6a2966475a0c01b379259c6bca3fbb0
rabrux <n3ur0n4@gmail.com>
2017-12-21 16:21:49 -0600

    collection lib helper

---
+++ src/lib/Collection.coffee
@@ -0,0 +1,34 @@
+class Collection
+
+  constructor : ( @_type, @_collection = [] ) ->
+    if not @_type
+      throw Error 'type needs to be specified on first argument of constructor'
+    if @_collection instanceof Array is false
+      throw Error 'collection must be an array'
+
+  set : ( @_collection ) ->
+  get : -> @_collection
+
+  findByKey : ( key, value ) ->
+    if not value
+      return @_collection.filter ( el ) -> el[ key ]
+
+    @_collection.filter ( el ) -> el[ key ] is value
+
+  register : ( item ) ->
+    if item instanceof @_type
+      @_collection.push item
+      return true
+    false
+
+  remove : ( items ) ->
+    if items instanceof Array
+      for i in items
+        @remove i
+    if items instanceof @_type
+      index = @_collection.indexOf items
+      @_collection.splice index, 1
+    @
+
+module.exports = Collection
+
---
+++ test/Collection.js
@@ -0,0 +1,65 @@
+var should     = require( 'should' );
+var Collection = require( '../dist/lib/collection' );
+
+// dummie data
+users = [
+  {
+    name : 'lorem',
+    pass : 'ipsum'
+  },
+  {
+    name : 'dolor',
+    pass : 'amet'
+  }
+];
+
+describe( 'Collection class', function() {
+  describe( 'construct collection instances', function() {
+    it( 'constructor without arguments should be an error', function() {
+      should( function() { return new Collection() } ).throw( 'type needs to be specified on first argument of constructor' );
+    } );
+
+    it( 'create collection of objects', function() {
+      ( new Collection( Object ) ).should.be.an.instanceOf( Collection );
+    } );
+
+    it( 'create collection with init items that isnt instance of array (error)', function() {
+      should( function() { return new Collection( Object, 'lorem' ) } ).throw( 'collection must be an array' );
+    } );
+
+    it( 'create collection with init items', function() {
+      ( new Collection( Object, users ) ).should.be.and.instanceOf( Collection );
+    } );
+  } );
+
+  describe( 'collection methods', function() {
+    it( 'get() should be equals to init data', function() {
+      ( new Collection( Object, users ) ).get().should.be.equals( users );
+    } );
+
+    it( 'findByKey( \'name\' ) needs to be an array with two elements', function() {
+      ( new Collection( Object, users ) ).findByKey( 'name' ).length.should.be.equal( 2 );
+    } )
+
+    it( 'findByKey( \'name\', \'lorem\' ) needs to be an array with one element', function() {
+      ( new Collection( Object, users ) ).findByKey( 'name', 'lorem' ).length.should.be.equal( 1 );
+    } );
+
+    it( 'register() is false if not instance of @_type (object)', function() {
+      ( new Collection( Object, users ) ).register( 'lorem' ).should.be.equal( false );
+    } );
+
+    it( 'register() success', function() {
+      ( new Collection( Object, users ) ).register( { name : 'rabrux', pass : 'access' } ).should.be.equal( true );
+    } );
+
+    it( 'remove() one element', function() {
+      var userCollection = new Collection( Object, users );
+      toRemove = userCollection.findByKey( 'name', 'lorem' );
+      userCollection.remove( toRemove ).get().length.should.be.equal( 1 );
+    } );
+
+  } );
+
+} );
+
